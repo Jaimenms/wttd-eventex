adb8f13a77f1f7bbccf1ce4cd04028cd03753afa
Jaime Souza <jnsouza@wit-ip.com>
2017-06-21 10:23:57 -0300

    Incluído campo paid

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -21,3 +21,7 @@ class SubscriptionModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Henrique Bastos', str(self.obj))
+
+    def test_paid_default_to_False(self):
+        """By default paid must be false"""
+        self.assertEqual(False, self.obj.paid)
