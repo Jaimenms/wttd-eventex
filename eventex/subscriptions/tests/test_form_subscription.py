54c16a2f0d7e50d254eb31e7d9e776d78de33434
Jaime Souza <jnsouza@wit-ip.com>
2017-06-22 13:58:16 -0300

    Nomes padronizados

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -22,6 +22,12 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_capitalized(self):
+        """Name must be capitalized"""
+        # HENRIQUE bastos   ->   Henrique Bastos
+        form = self.make_validated_form(name='HENRIQUE bastos')
+        self.assertEqual('Henrique Bastos', form.cleaned_data['name'])
+
 
     def assertFormErrorCode(self,form, field, code):
         errors = form.errors.as_data()
