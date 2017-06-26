da42d38478a354a9a7d0835f5e5f9e4f9fccc74e
Jaime Souza <jnsouza@wit-ip.com>
2017-06-26 11:37:01 -0300

    Mini-cursos

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from eventex.core.models import Speaker, Talk
+from eventex.core.models import Speaker, Talk, Course
 
 
 def home(request):
@@ -9,13 +9,13 @@ def home(request):
 
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speaker, slug=slug)
-
     return render(request,'core/speaker_detail.html', {'speaker': speaker})
 
-
 def talk_list(request):
+    speaker = Speaker(name='Henrique Bastos', slug='henrique-bastos')
     context = {
         'morning_talks': Talk.objects.at_morning(),
         'afternoon_talks': Talk.objects.at_afternoon(),
+        'courses': Course.objects.all()
     }
     return render(request,'core/talk_list.html', context)
